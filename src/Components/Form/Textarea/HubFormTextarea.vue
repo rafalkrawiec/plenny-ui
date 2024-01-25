c1e218908f494ef478b36b8d8edb3f0e81e50776
Rafał Krawiec <rafal.krawiec16@gmail.com>
2024-01-25 19:16:04 +0100

    Make textarea initial resize when component is mounted.

--- src/Components/Form/Textarea/HubFormTextarea.vue
+++ src/Components/Form/Textarea/HubFormTextarea.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { useControl, Control } from '../../../Composables/UseControl';
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
 
   defineOptions({
     inheritAttrs: false,
@@ -15,18 +15,22 @@
 
   const { control, input, model } = useControl({ props, emit });
 
-  const resize = () => {
+  function resize() {
     htmlControl.value.style.height = '0px';
     htmlControl.value.style.height = htmlControl.value.scrollHeight + 'px';
-  };
+  }
+
+  onMounted(() => {
+    resize();
+  });
 </script>
 <template>
   <HubFormControl class="control-textarea" v-bind="control">
     <template v-for="(_, slot) in $slots" v-slot:[slot]="props">
-      <slot :name="slot" v-bind="props"/>
+      <slot :name="slot" v-bind="props" />
     </template>
     <template v-slot:control>
-      <textarea ref="htmlControl" v-bind="{ ...input, ...$attrs }" v-model="model" @input="resize()"/>
+      <textarea ref="htmlControl" v-bind="{ ...input, ...$attrs }" v-model="model" @input="resize()" />
     </template>
   </HubFormControl>
 </template>
